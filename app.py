0f8b7e06d83dd17044606834467569313e9a6734
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-10 18:55:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ st.markdown("""
 # ----------------- HEADER -----------------
 st.markdown("""
 <div class="title-logo">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/512px-Logo_NIKE.svg.png" style="height:70px;">
+    <img src="C:\Users\prate\OneDrive\Desktop/nikelogo.png" style="height:70px;">
     <h1>Nike Stock Predictor</h1>
 </div>
 """, unsafe_allow_html=True)
